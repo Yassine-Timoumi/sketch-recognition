4a36d46d090ef8ed5c162dd520bae47031903236
Yassine-Timoumi <48143264+Yassine-Timoumi@users.noreply.github.com>
2020-09-29 13:17:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -24,4 +24,4 @@ description="This Convolution Neural Network was trained on Google's " \
             "QuickDraw dataset with 345 classes. Try it by drawing a " \
             "lightbulb, radio, or anything you can think of!"
 thumbnail="https://github.com/gradio-app/machine-learning-experiments/raw/master/lightbulb.png?raw=true"
-#gr.Interface(predict, "sketchpad", output, live=True, title=title,description=description, thumbnail=thumbnail, capture_session=True).launch()
+gr.Interface(predict, "sketchpad", output, live=True, title=title,description=description, thumbnail=thumbnail, capture_session=True).launch()
